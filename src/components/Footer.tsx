8bafb99f003e1c43bbfca54b14c1258fb7b20b15
Vedant Patel <patelvedant6296@gmail.com>
2024-09-11 17:47:44 +0530

    Add 'Developed by Vedant Patel' to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,12 @@ const Footer = () => {
         <span className="text-3xl text-white font-bold tracking-tight">
           DishDash
         </span>
+        <span className="text-white font-semibold mt-4 md:mt-0 tracking-tight text-center md:text-right">
+          <span>Developed by Vedant Patel</span>
+        </span>
         <span className="text-white font-bold tracking-tight flex gap-4">
           <span>About us</span>
-          <span>Contant us</span>
+          <span>Contact us</span>
         </span>
       </div>
     </div>
